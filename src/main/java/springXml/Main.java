249656b76dc62fa38be84cb493566fff2e9b210e
tomk <1245378160@qq.com>
2021-07-11 11:51:14 +0800

    提交代码到远程

--- src/main/java/springXml/Main.java
+++ src/main/java/springXml/Main.java
@@ -9,7 +9,6 @@ public class Main {
 
     //E:\apache-maven-3.8.1
 
-  //  E:\python;E:\python\Library\mingw-w64\bin;E:\python\Library\usr\bin;E:\python\Library\bin;E:\python\Scripts;E:\AppServ\Apache2.2\bin;E:\AppServ\php5;C:\Users\lenovo\AppData\Local\GitHubDesktop\bin;C:\Users\lenovo\AppData\Local\Programs\Microsoft VS Code\bin;%MAVEN_HOME%\bin\
 
     //C:\Program Files\Git\bin\git.exe
 
--- src/main/java/springXml/Main1.java
+++ src/main/java/springXml/Main1.java
@@ -1,9 +1,9 @@
 package springXml;
 
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("springXml")
 public class Main1 {
 
-    public static void main(String[] args) {
-
-        System.err.println("1");
-    }
 }
