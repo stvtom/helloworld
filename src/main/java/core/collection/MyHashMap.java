58070174250a70a96102832b3530ddc6e0c00612
tomk <1245378160@qq.com>
2021-07-26 00:03:07 +0800

    hash

--- src/main/java/core/collection/MyHashMap.java
+++ src/main/java/core/collection/MyHashMap.java
@@ -2,53 +2,76 @@ package core.collection;
 
 import java.util.*;
 
-public class MyHashMap<K,V>  implements Map<K,V> {
+public class MyHashMap<K,V> {
 
-    public int size() {
-        return 0;
+    private Entry<K,V>[] table;
+
+    private static final Integer  CAPACITY = 8;
+
+    private Integer size = 0;
+    public void put(K key,V value){
+        if (table ==  null){
+            inflate();
+        }
+        Integer hashcode = key.hashCode();
+        Integer index = indexFor(hashcode);
+
+        for (Entry<K,V> entry = table[index];entry != null ;entry = entry.next){
+            if (entry.key.equals(key)){
+                entry.value = value;
+            }
+        }
+
+        addEntry(key, value, index);
     }
 
-    public boolean isEmpty() {
-        return false;
+    private void addEntry(K key, V value, Integer index) {
+        Entry<K,V> entry = new Entry<>(key,value,table[index]);
+        table[index] = entry;
+        size++;
     }
 
-    public boolean containsKey(Object key) {
-        return false;
+    private Integer indexFor(Integer hashcode) {
+        return hashcode % table.length;
     }
 
-    public boolean containsValue(Object value) {
-        return false;
+    private void inflate() {
+        table = new Entry[CAPACITY];
     }
 
-    public V get(Object key) {
+    public V get(K key){
+        Integer hashcode = key.hashCode();
+        Integer index = indexFor(hashcode);
+
+        for (Entry<K,V> entry = table[index];entry != null ;entry = entry.next){
+            if (entry.key.equals(key)){
+                return entry.value;
+            }
+        }
         return null;
     }
 
-    public V put(K key, V value) {
-        return null;
+    public int size(){
+        return this.size;
     }
 
-    public V remove(Object key) {
-        return null;
-    }
 
-    public void putAll(Map<? extends K, ? extends V> m) {
+    class Entry<K,V>{
+        private K key;
+        private V value;
 
-    }
+        private Entry<K,V> next;
 
-    public void clear() {
-
-    }
-
-    public Set<K> keySet() {
-        return null;
-    }
-
-    public Collection<V> values() {
-        return null;
-    }
-
-    public Set<Entry<K, V>> entrySet() {
-        return null;
+        public Entry(K key,V value,Entry next){
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+        public K getKey() {
+            return key;
+        }
+        public V getValue() {
+            return value;
+        }
     }
 }
--- src/main/java/core/collection/MyHashMapTest.java
+++ src/main/java/core/collection/MyHashMapTest.java
@@ -4,7 +4,16 @@ public class MyHashMapTest {
 
     public static void main(String[] args) {
 
-        MyHashMap<String, String> hashMap = new MyHashMap<String, String>();
+        MyHashMap<Integer,String> myHashMap = new MyHashMap();
+
+        for (int i = 0; i <9 ; i++) {
+            myHashMap.put(i,i+"V");
+        }
+
+        System.err.println(myHashMap.get(1));
+
+        // 1.扩容
+        // 2.线程安全 fail_fast modCount
 
     }
 }
